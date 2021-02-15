86443bcaded8a06d0442a0d4888ac84082de900c
Daan Dalman <daandalman@gmail.com>
2021-02-15 21:13:02 +0100

    create first part of rail

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,37 @@
 class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
+    def show
+        id = params[:id]
+        @task = Task.find(id)
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        @task.save
+        redirect_to tasks_path
+    end
+
+    def edit
+        id = params[:id]
+        @task = Task.find(id)
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(params[:task])
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # As a user, I can list tasks
+  get '/tasks', to: 'tasks#index'
+
+  # As a user, I can add a new task
+    # display form
+  get '/tasks/new', to: 'tasks#new'
+    # post information into Database
+  post '/tasks', to: 'tasks#create'
+
+  # As a user, I can view the details of a task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  
+  # As a user, I can edit a task (mark as completed / update title & details)
+    # display the form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+    # update info into the database
+  post '/tasks/:id', to: 'tasks#update' 
+  # As a user, I can remove a tas
+  
 end
